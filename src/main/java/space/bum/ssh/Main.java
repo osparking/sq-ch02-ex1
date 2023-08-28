ddde7fdf8ca9a6069a2ead60bbf8e887438348b5
osparking <jbpark03@gmail.com>
2023-08-28 11:24:28 +0900

    broken UTF-8 on console error fix

--- src/main/java/space/bum/ssh/Main.java
+++ src/main/java/space/bum/ssh/Main.java
@@ -1,9 +1,10 @@
 package space.bum.ssh;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.PrintStream;
+
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import space.bum.ssh.config.AppConfig;
@@ -11,20 +12,15 @@ import space.bum.ssh.config.AppConfig;
 public class Main {
 
 	public static void main(String[] args) {
-		var context = new AnnotationConfigApplicationContext(AppConfig.class);
-		
-		Parrot p = (Parrot) context.getBean("parrot");
-		System.out.println(p.getName());
-		
-    System.setOut(new PrintStream(
-    		new FileOutputStream(FileDescriptor.out), 
-    		true, StandardCharsets.UTF_8));
-    
-		var msg = (String) context.getBean("message");
-		System.out.println(msg);
-		
-		var unit = (Integer) context.getBean("unit");
-		System.out.println(unit);
+		try (
+				var context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+			PrintStream out = new PrintStream(System.out, true, UTF_8);
+
+			out.println((String) context.getBean("message"));
+			out.println((Integer) context.getBean("unit"));
+		} catch (BeansException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
