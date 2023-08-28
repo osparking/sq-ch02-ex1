d93dfc1ebd12c2c4355d352ed25dafaa1449aebf
osparking <jbpark03@gmail.com>
2023-08-28 10:38:37 +0900

    two more beans added to context

--- src/main/java/space/bum/ssh/config/AppConfig.java
+++ src/main/java/space/bum/ssh/config/AppConfig.java
@@ -8,6 +8,16 @@ import space.bum.ssh.Parrot;
 @Configuration
 public class AppConfig {
 	@Bean
+	String message() {
+		return "10월 5일 쾌청 기원";
+	}
+	
+	@Bean
+	Integer unit() {
+		return 1004;
+	}
+	
+	@Bean
 	Parrot parrot() {
 		var rookie = new Parrot();
 		rookie.setName("Rookie");
