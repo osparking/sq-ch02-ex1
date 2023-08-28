87738f86d85dae5e2663af97c2c2e0f7d2b52c97
osparking <jbpark03@gmail.com>
2023-08-28 09:10:16 +0900

    parrot bean method added

--- src/main/java/space/bum/ssh/config/AppConfig.java
+++ src/main/java/space/bum/ssh/config/AppConfig.java
@@ -1,8 +1,16 @@
 package space.bum.ssh.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import space.bum.ssh.Parrot;
 
 @Configuration
 public class AppConfig {
-
+	@Bean
+	Parrot parrot() {
+		var rookie = new Parrot();
+		rookie.setName("Rookie");
+		return rookie;
+	}
 }
