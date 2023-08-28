ecd750081d0ef7dd032b645f8d5672aff0852348
osparking <jbpark03@gmail.com>
2023-08-28 09:40:16 +0900

    context made use config class

--- src/main/java/space/bum/ssh/Main.java
+++ src/main/java/space/bum/ssh/Main.java
@@ -1,12 +1,13 @@
 package space.bum.ssh;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import space.bum.ssh.config.AppConfig;
+
 public class Main {
 
 	public static void main(String[] args) {
-		var context = new AnnotationConfigApplicationContext();
-		
-		Parrot p = new Parrot();
+		var context = new AnnotationConfigApplicationContext(AppConfig.class);
+
 	}
 
 }
