43845dad8cec8f37c1b5a05347133a36f9925902
osparking <jbpark03@gmail.com>
2023-08-28 10:40:42 +0900

    verify if 2 more beans are in context

--- src/main/java/space/bum/ssh/Main.java
+++ src/main/java/space/bum/ssh/Main.java
@@ -1,4 +1,9 @@
 package space.bum.ssh;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import space.bum.ssh.config.AppConfig;
@@ -8,8 +13,18 @@ public class Main {
 	public static void main(String[] args) {
 		var context = new AnnotationConfigApplicationContext(AppConfig.class);
 		
-		Parrot p = (Parrot) context.getBean(Parrot.class);
+		Parrot p = (Parrot) context.getBean("parrot");
 		System.out.println(p.getName());
+		
+    System.setOut(new PrintStream(
+    		new FileOutputStream(FileDescriptor.out), 
+    		true, StandardCharsets.UTF_8));
+    
+		var msg = (String) context.getBean("message");
+		System.out.println(msg);
+		
+		var unit = (Integer) context.getBean("unit");
+		System.out.println(unit);
 	}
 
 }
